5d86357f05126de408cc7544f3dfbffd1d6b5f2e
Dennis Chan Paralejas <70734473+dparalejas04@users.noreply.github.com>
2023-02-14 17:24:57 +0800

    add design for sidebar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,4 +10,37 @@ export class DashboardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  // sample: boolean = false;
+
+  sample: any = {
+    dashboard: true,
+    employees: false,
+  };
+
+  public select(sample: string) {
+    this.sample = !this.sample;
+
+    switch (sample) {
+      case 'dashboard': {
+        alert();
+        //statements;
+        for (const key in this.sample) {
+          this.sample[key] = false;
+        }
+
+        this.sample.dashboard = true;
+        break;
+      }
+      default: {
+        this.setAllValuesToFalse();
+        this.sample.employees = true;
+        break;
+      }
+    }
+  }
+
+  setAllValuesToFalse(): void {
+    this.sample.dashboard = false;
+  }
 }
